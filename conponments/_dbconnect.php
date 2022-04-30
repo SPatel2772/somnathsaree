4895b013c206edd7a5f645f405f9ca221c7d58a8
Shyam Borsaniya <71972054+SPatel2772@users.noreply.github.com>
2022-04-30 08:47:02 +0530

    Add files via upload

---
+++ conponments/_dbconnect.php
@@ -0,0 +1,20 @@
+<?php
+
+    // connect to a database
+    $servername = "localhost";
+    $username = "root";
+    $passwoard = "";
+    $database = "somnathsaree";
+
+    $conn = mysqli_connect($servername, $username, $passwoard, $database);
+
+    /*if ($conn)
+    {
+        echo "Connection successful...";
+    }
+    else
+    {
+        echo "Connection error...";
+    }*/
+
+?>
---
+++ conponments/_footer.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/all_style.css" type="text/css">
+</head>
+
+<body>
+
+    <div class="container-fluid" id="footer-color">
+        <div class="container pt-2">
+            <div class="row">
+                <div class="col-6">
+                    <p style="font-size:29px;">Contact us</p>
+                    <i class="fa fa-whatsapp  mb-3" style="font-size:22px"></i> &nbsp; &nbsp;<i class="fa fa-phone"style="font-size:22px"></i>
+                    <p style="font-size:17px;">Hetalben Kalola &nbsp; +91 1234567890</p>
+                    <p style="font-size:17px;" >Jitubhai Kalola  &nbsp; +91 1234567890</p>
+                </div>
+                <div class="col-6">
+                    <p style="font-size:29px;">Address</p>
+                    <p>Somnath Saree<br>S.B.S. Road, Anand Park,<br>Manavader - 362630<br>Dist. Junagadh</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
+        </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
